5e8151c20770cc46d08d2d05d45531920d733dde
BrandonCope <BrandonCope>
2022-09-28 22:29:33 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Alta3 Research | BCopeland
+    Lists of Lists Optional Challenge"""
+
+def main():
+    animals = ["Fox", "Bee", "Cat", "Dog", "Cow"]
+    for i in animals:
+        print(i, end=" ")
+    print(end="\n")
+    
+main()
